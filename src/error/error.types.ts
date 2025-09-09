a37048e4f74a3031881e2bd30a9bccbaf19d0cd7
Matthew Maxwell <maxwellmattryan@gmail.com>
2025-09-09 18:39:17 -0500

    Remove example

--- src/error/error.types.ts
+++ src/error/error.types.ts
@@ -2,17 +2,5 @@
  * Wraps a functions return type to account for possible errors that
  * may occur during its execution. If a function is likely to throw errors,
  * using this type is preferred.
- *
- * @example
- * ```
- * function divide(a: number, b: number): Result<number>
- *
- * const [result, error] = divide(1, 0)
- * if (error) {
- *     // Do something with the error
- * } else {
- *     // Do something with the result
- * }
- * ```
  */
 export type Result<T, E = Error> = [T, null] | [null, E]
