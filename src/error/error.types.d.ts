b86e63d669f87214399e32f9c44fa3d979a353d2
Matthew Maxwell <maxwellmattryan@gmail.com>
2025-09-09 13:50:11 -0500

    Remove type decs

--- src/error/error.types.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Wraps a functions return type to account for possible errors that
- * may occur during its execution. If a function is likely to throw errors,
- * using this type is preferred.
- */
-export type Result<T, E = Error> = [T, null] | [null, E]
